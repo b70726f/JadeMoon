724cbc97b608a3dd2a9aa1799b762d706aba8f4e
b70726f <b70726f@gmail.com>
2024-01-27 18:23:16 +0100

    Add SourceFile library

---
+++ src/source/source_file.cpp
@@ -0,0 +1,68 @@
+#include "source/source_file.hpp"
+
+SourceFile::SourceFile(std::filesystem::path const path)
+    : lines_details_ {}
+    , path_ { path }
+    , source_ {}
+{
+}
+
+char SourceFile::at(u32 index) const
+{
+    return source_.at(index);
+}
+
+u32 SourceFile::length() const
+{
+    return source_.size();
+}
+
+std::string_view SourceFile::line(u16 const line_number) const
+{
+    auto [line_start_offset, line_length] = lines_details_.at(line_number);
+
+    return { source_.data() + line_start_offset, line_length };
+}
+
+void SourceFile::load()
+{
+    std::ifstream file {};
+    file.open(path_, std::ios::in);
+
+    if (!file.good()) {
+        throw std::runtime_error { "Could not open a file" };
+    }
+
+    source_ = load_file_to_memory(file);
+    file.close();
+
+    make_metadata();
+}
+
+std::vector<char> SourceFile::load_file_to_memory(std::ifstream& file)
+{
+    return { std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>() };
+}
+
+void SourceFile::make_metadata()
+{
+    u32 line_start_offset = 0;
+    u32 file_offset = 0;
+
+    for (auto character : source_) {
+        bool eol = (character == '\n');
+        bool eof = (file_offset == source_.size() - 1);
+
+        if (eol || eof) {
+            u16 line_length = file_offset - line_start_offset;
+
+            if (eof)
+                line_length += 1;
+
+            lines_details_.push_back(LineData { line_start_offset, line_length });
+            line_start_offset = file_offset + 1;
+        }
+
+        ++file_offset;
+    }
+}
---
+++ src/source/source_file.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <limits>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include "jm_int.hpp"
+#include "source/i_source.hpp"
+
+class SourceFile : public ISource {
+    using LineData = std::pair<u16 const, u16 const>;
+    using LineDetails = std::vector<LineData>;
+
+public:
+    ~SourceFile() = default;
+    SourceFile(std::filesystem::path const);
+
+    char at(u32 index) const override;
+    u32 length() const;
+    std::string_view line(u16 const) const override;
+    void load();
+
+private:
+    inline std::vector<char> load_file_to_memory(std::ifstream& file);
+    inline void make_metadata();
+
+    LineDetails lines_details_;
+    std::filesystem::path const path_;
+    std::vector<char> source_;
+};
