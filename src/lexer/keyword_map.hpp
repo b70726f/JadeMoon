064dc40d098b3f469471b01f3a4a545ba43f3387
b70726f <b70726f@gmail.com>
2024-02-27 21:59:31 +0100

    [Lexer, KewordMap] Change type of KeywordMap to std::array

--- src/lexer/keyword_map.hpp
+++ src/lexer/keyword_map.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <string_view>
 #include <utility>
 
@@ -7,15 +8,17 @@
 
 class KeywordMap {
     friend class Lexer;
+    using Keyword = std::pair<std::string_view, TokenKind>;
 
 private:
-    inline static std::pair<std::string_view, TokenKind> map_[] = {
-        { "fn", TokenKind::kw_fn },
-        { "var", TokenKind::kw_var },
-        { "while", TokenKind::kw_while },
-        { "if", TokenKind::kw_if },
-        { "return", TokenKind::kw_return },
-        { "else", TokenKind::kw_else },
-        { "struct", TokenKind::kw_struct }
+    static constexpr u8 KEYWORD_COUNT = 7;
+    static constexpr std::array<Keyword, KEYWORD_COUNT> map_[] = {
+        std::make_pair("fn", TokenKind::kw_fn),
+        std::make_pair("var", TokenKind::kw_var),
+        std::make_pair("while", TokenKind::kw_while),
+        std::make_pair("if", TokenKind::kw_if),
+        std::make_pair("return", TokenKind::kw_return),
+        std::make_pair("else", TokenKind::kw_else),
+        std::make_pair("struct", TokenKind::kw_struct)
     };
 };
