46fe04a1821c95b5b621bc54d4b476bf853e9cfe
b70726f <b70726f@gmail.com>
2024-03-03 09:49:29 +0100

    Add main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include "jm_int.hpp"
+#include <iostream>
 
-i32 main(i32 argc, char* argv[])
+#include "jmc/types.hpp"
+#include "lexer/lexer.hpp"
+#include "source/source_file.hpp"
+
+i32 main([[maybe_unused]] i32 argc, [[maybe_unused]] char* argv[])
 {
+    SourceFile sf { "testfile" };
+    sf.load();
+    auto source = sf.source();
+    Lexer lex { source };
+    auto tokens = lex.analise();
+
+    std::cout << tokens.capacity();
+    for (auto const& token : tokens) {
+        std::cout << token.literal() << std::endl;
+    }
+
     return 0;
 }
