e253a71c62808f638b1fd4e42f30e459841c7005
b70726f <b70726f@gmail.com>
2024-03-06 19:03:01 +0100

    Add simle draft of command line

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
+#include "command_line/command_line.hpp"
 #include "jmc/types.hpp"
 #include "lexer/lexer.hpp"
 #include "source/source_file.hpp"
 
-i32 main([[maybe_unused]] i32 argc, [[maybe_unused]] char* argv[])
+i32 main(i32 argc, char const* argv[])
 {
+    CommandLine cl(utils::c_string_array_to_string_vector(argc - 1, argv + 1));
+    utils::initialize_cli_options(cl);
+
     SourceFile sf { "testfile" };
     sf.load();
     auto source = sf.source();
