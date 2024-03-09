685eb24a5205d5322a82a925aca1f8a0f715cd0d
b70726f <b70726f@gmail.com>
2024-03-09 15:59:42 +0100

    [CommandLine] Add working implementation

--- src/command_line/command_line.hpp
+++ src/command_line/command_line.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <algorithm>
 #include <string>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include "command_line/option_builder.hpp"
@@ -14,12 +17,22 @@ void initialize_cli_options(CommandLine&);
 } // namspace utils
 
 class CommandLine {
+    using OptionalArguments = std::optional<std::vector<std::string>>;
+
 public:
     CommandLine(std::vector<std::string> const&);
     void parse();
     void register_option(Option const&);
+    OptionalArguments remaining() const;
+    OptionalArguments option_arguments(std::string const&) const;
+    bool is_option_present(std::string const&) const;
 
 private:
+    bool regular_option_();
+    bool reserved_option_();
+
+    std::unordered_map<std::string, OptionalArguments> provided_options_;
     std::vector<Option> options_;
+    std::vector<std::string> remaining_;
     SourceHandler<std::string> handler_;
 };
